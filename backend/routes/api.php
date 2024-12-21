fa81999be3bde88e011cd8d8739d3036b4cfe513
B4lint0413 <balu050413@gmail.com>
2024-12-21 21:28:51 +0000

    Routing

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,4 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GuitarController;
 
+Route::get('/guitars', [GuitarController::class, 'index'])->name('guitars.index');
+Route::get('/guitars/{id}', [GuitarController::class, 'show'])->whereNumber('id')->name('guitars.show');
+Route::post('/guitars', [GuitarController::class, 'store'])->name('guitars.store');
+Route::put('/guitars/{id}', [GuitarController::class, 'update'])->whereNumber('id')->name('guitars.update');
+Route::delete('/guitars/{id}', [GuitarController::class, 'destroy'])->whereNumber('id')->name('guitars.destroy');
